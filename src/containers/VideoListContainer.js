2c16df5080288ee969da95ca5ccebe2155d40728
danielharmon <dannyaharmon@gmail.com>
2020-10-12 20:02:53 -0700

    Complete replacing components with containers

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,9 +2,24 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+var mapStatetoProps = function(state) {
+  return {
+    videos: state.videos
+  }
+}
+
+var mapDispatchToProps = function() {
+
+}
+
+var VideoListContainer = () => {
+
+
+
+};
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
 
 export default VideoListContainer;
+
